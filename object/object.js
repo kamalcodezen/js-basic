e7e7cec3506e0da11a8637288c52626d24250353
kamalcodezen <kamaluddin7908@gmail.com>
2026-01-04 00:53:07 +0530

    practice 1st object print and object typeof 1 - 15 line

--- object/object.js
+++ object/object.js
@@ -1,1 +1,15 @@
-console.log("kamaluddin")
+
+// object practice begginer
+const student = {
+    fullName: "kamal",
+    age: 26,
+    city: "delhi",
+    isPass: true,
+}
+console.log(student);
+// typeof all
+console.log("student = ", typeof student);
+console.log("fullName = ", typeof student.fullName);
+console.log("age = ", typeof student.age);
+console.log("student = ", typeof student.city);
+console.log("student = ", typeof student.isPass);
