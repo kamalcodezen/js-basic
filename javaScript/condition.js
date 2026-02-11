9425124161183eb14b856ad2f3c97565bdf3e09c
kamalcodezen <kamaluddin7908@gmail.com>
2026-02-12 02:13:49 +0530

    prime number check

--- javaScript/condition.js
+++ javaScript/condition.js
@@ -26,3 +26,34 @@ for (let i = 1; i <= 50; i++) {
     }
 }
 console.log(sumEven);
+
+console.log(`=--------=-------=`);
+
+// Check prime number
+let number = 13;
+let isPrime = true;
+for (let i = 2; i < number; i++) {
+    if (number % i === 0) {
+        isPrime = false;
+    }
+}
+console.log(isPrime);
+
+
+console.log(`=====------------`);
+
+
+// Print prime numbers 1–50
+
+for (let i = 2; i <= 50; i++) {
+    let isPrime = true;
+    for (let j = 2; j < i; j++) {
+        if (i % j === 0) {
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime) {
+        console.log(i);
+    }
+}
