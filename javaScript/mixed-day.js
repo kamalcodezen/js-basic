227c79fe38dc081c50900c257ab8b4c258f69d0f
kamalcodezen <kamaluddin7908@gmail.com>
2026-02-13 00:43:17 +0530

    reverse way array

--- javaScript/mixed-day.js
+++ javaScript/mixed-day.js
@@ -12,3 +12,20 @@ for (let name of names) {
 console.log(newNames);
 
 
+// Reverse array
+
+let revArray = [];
+
+for (let name of names) {
+    revArray.unshift(name);
+}
+console.log(revArray);
+
+
+let revName = [...names].reverse();
+console.log(revName);
+
+
+for (let i = names.length - 1; i >= 0; i--){
+    console.log(names[i])
+}
