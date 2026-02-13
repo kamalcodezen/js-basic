d9f8fdf11f3f0ca840c675b3c5383929ffd7ced1
kamalcodezen <kamaluddin7908@gmail.com>
2026-02-13 12:48:03 +0530

    Count frequency  practice

--- javaScript/array-control.js
+++ javaScript/array-control.js
@@ -16,3 +16,38 @@ for (let number of numbers) {
 
 console.log(smallNumber);
 console.log(secondSmallestNum);
+
+
+console.log(`---------------------`);
+
+
+// Remove duplicates
+
+const nums = [22, 12, 34, 12, 65, 45, 65, 87, 34, 87];
+
+let newNums = [];
+for (let num of nums) {
+    if (newNums.includes(num) === false) {
+        newNums.push(num);
+    }
+}
+console.log(newNums);
+
+
+
+console.log(`------------------------`);
+
+// Count frequency of each element
+
+const names = ["a", "b", "a", "c", "b", "a","d"];
+
+let frequency = {};
+
+for (let name of names) {
+    if (frequency[name]) {
+        frequency[name]++;
+    } else {
+        frequency[name] = 1;
+    }
+}
+console.log(frequency);
