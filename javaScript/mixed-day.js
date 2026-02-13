b4c30255d15e30f8b5dfdd84f6dd1d4c911e4309
kamalcodezen <kamaluddin7908@gmail.com>
2026-02-13 10:50:44 +0530

    find second largest number

--- javaScript/mixed-day.js
+++ javaScript/mixed-day.js
@@ -26,6 +26,45 @@ let revName = [...names].reverse();
 console.log(revName);
 
 
-for (let i = names.length - 1; i >= 0; i--){
+for (let i = names.length - 1; i >= 0; i--) {
     console.log(names[i])
 }
+
+
+console.log(`-----------------`);
+
+
+
+// Find second largest number
+const numbers = [23, 35, 56, 34, 24, 63, 84, 22];
+
+let largestNum = -Infinity;
+let secondLargestNum = -Infinity;
+
+for (let number of numbers) {
+    if (number > largestNum) {
+        secondLargestNum = largestNum;
+        largestNum = number;
+    } else if (number > secondLargestNum) {
+        secondLargestNum = number;
+    }
+}
+console.log(secondLargestNum);
+
+
+
+const nums = [10, 10, 5];
+
+let largest = -Infinity;
+let secondLargest_Num = -Infinity;
+
+for (let num of nums) {
+
+    if (num > largest) {
+        secondLargest_Num = largest;
+        largest = num;
+    } else if (num > secondLargest_Num && num !== largest) {
+        secondLargest_Num = num;
+    }
+}
+console.log(secondLargest_Num)
