3b7e20ada68046bab59376a0a1b0370b4c73b3ab
kamalcodezen <kamaluddin7908@gmail.com>
2026-02-13 14:38:43 +0530

    frequency word practice and capitalize first letter

---
+++ javaScript/string-upgrade.js
@@ -0,0 +1,54 @@
+// Find longest word in sentence
+
+let details = "i am a good boy";
+
+let convertArray = details.trim().split(/\s+/);
+
+
+let longestWord = convertArray[0];
+for (let i of convertArray) {
+    if (i.length > longestWord.length) {
+        longestWord = i;
+    }
+}
+console.log(longestWord);
+
+
+
+console.log(`---------------------`);
+
+
+
+// Capitalize first letter of each word
+
+const cities = ["delhi", "pune", "mumbai", "delhi", "kolkata", "kerala"];
+
+let newNameCities = [];
+for (let city of cities) {
+    let newName = city[0].toUpperCase() + city.slice(1);
+    newNameCities.push(newName);
+}
+
+console.log(newNameCities);
+
+
+
+console.log(`---------------------`);
+
+// Count frequency of word
+
+let frequency = {};
+for (let city of cities) {
+    if (frequency[city]) {
+        frequency[city]++;
+    } else {
+        frequency[city] = 1;
+    }
+}
+console.log(frequency);
+
+
+console.log(`---------------------`);
+
+
+
