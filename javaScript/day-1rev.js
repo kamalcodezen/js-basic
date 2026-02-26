657fb5189be9b8ef1267a8acde420d9c6b525c3a
kamalcodezen <kamaluddin7908@gmail.com>
2026-02-27 01:26:35 +0530

    input dom show

--- javaScript/day-1rev.js
+++ javaScript/day-1rev.js
@@ -1,62 +1,27 @@
-// 🔁 Revision
-
-// Print 1–10
-
-// Even / Odd
-
-// 🆕 New
-
-// Count vowels
-
-// Find largest in array
-
-// 🌐 DOM
-
-// Input → vowel count show
-
-let number = 10;
-for (let i = 1; i <= number; i++) {
-    console.log(i);
-}
-
-console.log(`--------------------------`);
-
-for (let i = 1; i <= number; i++) {
-    if (i % 2 === 0) {
-        console.log(i)
-    }
-}
-
-console.log(`-----------------`);
-
-document.getElementById("submit-btn")
+const vowelBtn = document.getElementById("vowel-btn")
     .addEventListener("click", function () {
-        const userInput = document.getElementById("user-input");
-        let inputValue = userInput.value;
-        let result = countVowel(inputValue);
 
-        const totalValue = document.querySelector(".vowel-result");
-        totalValue.innerText = result;
+        const inputVowel = document.getElementById("count-vowel");
 
-    });
+        let vowelValue = inputVowel.value;  // 
+        // console.log(vowelValue);
+        let result = countVowel(vowelValue); //noyon
 
-function countVowel(text) {
-    /*  text = text.toLowerCase();
-      let count = 0;
-      for (let t of text) {
-          if (t === "a" || t === "e" || t === "i" || t === "o" || t === "u") {
-              count++;
-          }
-      }
-      return count;   */
-    let vowel = "aeiou";
-    text = text.toLowerCase();
-    let count = 0;
-    for (let char of text) {
-        if (vowel.includes(char)) {
-            count++;
+        const newDiv = document.createElement("div");
+        let para = document.createElement("p");
+        para.innerText = result;
+
+        newDiv.appendChild(para);
+        document.body.appendChild(newDiv);
+
+
+        function countVowel(text) {
+            let count = 0;
+            for (let letter of text) {
+                if (letter === "a" || letter === "e" || letter === "i" || letter === "o" || letter === "u") {
+                    count++;
+                }
+            }
+            return count;
         }
-    }
-    return count;
-}
-
+    })
