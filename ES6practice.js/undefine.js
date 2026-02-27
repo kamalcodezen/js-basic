6761503e8e9af4f116f695907f91a5cd87a43898
kamalcodezen <kamaluddin7908@gmail.com>
2026-02-27 16:57:55 +0530

    undefinr check

---
+++ ES6practice.js/undefine.js
@@ -0,0 +1,24 @@
+let a;
+console.log(a);
+
+let num = [1, 4, 6, 3];
+console.log(num[6]);
+delete num[2]
+console.log(num);
+console.log(num[2]);
+
+let person = {
+    name: "kamal"
+}
+
+console.log(person.age);
+
+console.log(`----------`);
+
+function test() {
+    let result = 3 * 6;
+    // return ;
+}
+
+let result = test();
+console.log(result);
