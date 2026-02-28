d92691face127e6401e5027ea7249019adef2949
kamalcodezen <kamaluddin7908@gmail.com>
2026-02-28 21:04:33 +0530

    global scope and block scope check

---
+++ ES6practice.js/scope.js
@@ -0,0 +1,33 @@
+// 1.global scope  -  2.block scope  -  3.function scope;
+
+
+
+
+//  1.global scope var - let - const
+
+var name = "kamal"; // var global scope
+
+let data = 4; //let global scope
+
+const fullName = "kamal"; // const global scope 
+
+for (let n of name) {
+    console.log(fullName);
+};
+
+
+
+// 2.block scope  let and const
+
+{
+    var state = "kolkata";
+    let city = "Delhi";
+    const product = "MI";
+}
+// console.log(state); // var is a not block scope
+
+// console.log(city); // let block scope
+
+// console.log(product); // const block scope
+
+
