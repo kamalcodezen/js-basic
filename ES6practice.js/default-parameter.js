f7ce167287962b97fb3da9a4131e9748adf89bd4
kamalcodezen <kamaluddin7908@gmail.com>
2026-02-28 17:22:31 +0530

    deafault parameter practice

---
+++ ES6practice.js/default-parameter.js
@@ -0,0 +1,10 @@
+
+function defaultParameter(a = 0, b = 0) {
+    return add = a + b
+}
+
+console.log(defaultParameter(12));
+console.log(defaultParameter());
+console.log(defaultParameter(12, 5));
+console.log(defaultParameter(undefined, 5));
+console.log(defaultParameter(null, 8));
