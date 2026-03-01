1eb6b2ee6422f492ed6906e7daceb404b274609f
kamalcodezen <kamaluddin7908@gmail.com>
2026-03-01 12:24:46 +0530

    arrow function practice

---
+++ ES6practice.js/arrow-function.js
@@ -0,0 +1,14 @@
+
+let multiply = (a, b) => a * b;
+
+console.log(multiply(2, 5));
+
+let square = a => a * a;
+console.log(square(5));
+
+
+let sum = (x, y) => {
+    let result = x + y;
+    return result;
+}
+console.log(sum(2, 5));
